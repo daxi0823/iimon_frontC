a674f3d36966e292d9cde5a0e0c9f835ff27c3cf
daxi0823 <daxi0823@gmail.com>
2022-11-03 18:39:35 +0900

    不要行削除

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,16 +5,14 @@
     <div class="buttonWrapper">
       <div>
         <button class="buttonX">x</button>
-        
+
       </div>
     </div>
   </container>
 </template>
 
 <script>
-export default {
-  components: { InputForm },
-  
+export default {  
 }
 </script>
 
