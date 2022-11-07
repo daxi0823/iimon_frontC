c3c026f1cd2536e0048dce4767f2327ee44054b0
daxi0823 <daxi0823@gmail.com>
2022-11-07 13:59:10 +0900

    css修正

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -43,46 +43,14 @@ export default {
   width: 300px;
   border: 1px solid navy;
 
-  display: flex;
   align-items: center;
 }
-.inputBox {
-  box-sizing: border-box;
-  height: 30px;
-  border: 1px solid navy;
-  margin-right: 10px;
-}
+
 .submitButton {
   box-sizing: border-box;
   height: 30px;
   padding: 3px;
   background-color: navy;
   color: white;
-}
-/* item部分 */
-.itemContainer {
-  display: flex;
-  align-items: center;
-  box-sizing: border-box;
-  height: 50px;
-  border: 1px solid navy;
-  margin-bottom: 3px;
-}
-.buttonX {
-  box-sizing: border-box;
-  height: 50px;
-  width: 50px;
-  border-right: 1px solid navy;
-  background-color: navy;
-  color: white;
-}
-.items {
-  display: inline-block;
-  box-sizing: border-box;
-  width: 250px;
-}
-.itemFont {
-  font-size: 16px;
-  padding-left: 20px;
 }
 </style>
