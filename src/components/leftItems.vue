5c03d57df4755e0e7569b844d22f7f3ce2164d12
daxi0823 <daxi0823@gmail.com>
2022-11-07 13:41:33 +0900

    left分作成

---
+++ src/components/leftItems.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in leftItems" :key="index">
+        {{item.name}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+    leftItems() {
+      return this.$store.getters.leftItems;
+    },
+  }
+}
+</script>
+
+
