94c2d36f5fd064461d49e84a9efbff0967331688
daxi0823 <daxi0823@gmail.com>
2022-11-09 10:58:22 +0900

    css修正

--- src/components/rightBox.vue
+++ src/components/rightBox.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
-    <ul>
-      <li v-for="(item, index) in rightItems" :key="index">
-        <div class="items">{{item}}</div>
-      </li>
-    </ul>
-    <!-- ボタン -->
-    <input type="button" class="submitButton" @click="rightButton" value="左へ移動">
+    <div class="mainWrapper">
+      <div class="itemContainer" v-for="(item, index) in leftItems" :key="index">
+        <span class="itemBlock">{{item}}</span>
+      </div>
+    </div>
+    <input type="button" class="submitButton" @click="leftButton" value="右へ移動">
   </div>
 </template>
 
@@ -14,24 +13,51 @@
 export default {
   data(){
     return{
-
     }
   },
   methods: {
-    rightButton() {
-      this.$store.commit('rightButton')
+    leftButton() {
+      this.$store.commit('leftButton')
     }
   },
   computed: {
-    rightItems() {
-      return this.$store.state.rightItems;
+    leftItems() {
+      return this.$store.state.leftItems;
     },
   }
 }
 </script>
 
 <style scoped>
-
-
-
+.mainWrapper {
+  width: 300px;
+  height: 500px;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  box-sizing: border-box;
+  border: 1px solid navy;
+}
+.itemContainer {
+  width: 80%;
+}
+.itemBlock {
+  display: block;
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  border-bottom: 1px solid navy;
+  text-align: center;
+  font-size: 16px;
+}
+.submitButton {
+  display: block;
+  box-sizing: border-box;
+  width: 150px;
+  height: 40px;
+  padding: 3px;
+  margin: 30px auto;
+  background-color: navy;
+  color: white;
+}
 </style>
