047da84730752f856a0ce707511a8b32adce4d86
daxi0823 <daxi0823@gmail.com>
2022-11-09 10:56:52 +0900

    css 修正

--- src/components/leftIBox.vue
+++ src/components/leftIBox.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="mainWrapper">
-      <div v-for="(item, index) in leftItems" :key="index">
-        <div class="item">{{item}}</div>
+      <div class="itemContainer" v-for="(item, index) in leftItems" :key="index">
+        <span class="itemBlock">{{item}}</span>
       </div>
     </div>
     <input type="button" class="submitButton" @click="leftButton" value="右へ移動">
@@ -13,7 +13,6 @@
 export default {
   data(){
     return{
-
     }
   },
   methods: {
@@ -32,25 +31,32 @@ export default {
 <style scoped>
 .mainWrapper {
   width: 300px;
-  height: 800px;
+  height: 500px;
   display: flex;
   flex-flow: column;
   align-items: center;
   box-sizing: border-box;
   border: 1px solid navy;
 }
-.item{
+.itemContainer {
   width: 80%;
+}
+.itemBlock {
+  display: block;
+  width: 100%;
   height: 50px;
-  text-align: center;
-  padding: auto;
-  font-size: 16px;
+  line-height: 50px;
   border-bottom: 1px solid navy;
+  text-align: center;
+  font-size: 16px;
 }
 .submitButton {
+  display: block;
   box-sizing: border-box;
-  height: 30px;
+  width: 150px;
+  height: 40px;
   padding: 3px;
+  margin: 30px auto;
   background-color: navy;
   color: white;
 }
